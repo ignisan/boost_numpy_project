74912cd5e63818206076b61a311c708d6f288358
ignisan <github@ignisan.info>
2016-09-28 09:35:20 +0900

    add eigen header helper

---
+++ eigen.hpp
@@ -0,0 +1,9 @@
+#pragma once
+//#define EIGEN_DEFAULT_TO_ROW_MAJOR
+//#define EIGEN_DEFAULT_DENSE_INDEX_TYPE long long
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#pragma GCC diagnostic ignored "-Wignored-attributes"
+#include<Eigen/Core>
+#include<Eigen/Sparse>
+#pragma GCC diagnostic pop
--- to_eigen_map.hpp
+++ to_eigen_map.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include"eigen.hpp"
 #include<boost/numpy.hpp>
 
 template<class T>
